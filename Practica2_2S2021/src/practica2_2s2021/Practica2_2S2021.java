cb1841d6d48368957b5af1c26f50efe36e92551f
Adriana Ojeda <88566130+Adriluuu@users.noreply.github.com>
2021-12-17 23:03:56 -0600

    Add files via upload

---
+++ Practica2_2S2021/src/practica2_2s2021/Practica2_2S2021.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2_2s2021;
+
+/**
+ *
+ * @author 50242
+ */
+public class Practica2_2S2021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
